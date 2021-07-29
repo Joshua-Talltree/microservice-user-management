685229eb5a5b42814302f41cd36b56e260542648
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-07-28 22:11:22 -0500

    feat: updated user controller to include discovery client and updated the application file to run discovery service as well.

--- src/main/java/com/josh/microserviceusermanagement/controller/UserController.java
+++ src/main/java/com/josh/microserviceusermanagement/controller/UserController.java
@@ -4,6 +4,9 @@ import com.josh.microserviceusermanagement.model.Role;
 import com.josh.microserviceusermanagement.model.User;
 import com.josh.microserviceusermanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +23,33 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private Environment env;
+
+    @Value("${spring.application.name}")
+    private String serviceId;
+
+    @GetMapping("/service/port")
+    public String getPort() {
+        return "Service port number : " + env.getProperty("local.server.port");
+    }
+
+    @GetMapping("/service/instances")
+    public ResponseEntity<?> getInstances() {
+        return new ResponseEntity <>(discoveryClient.getInstances(serviceId), HttpStatus.OK);
+    }
+
+    @GetMapping("/service/services")
+    public ResponseEntity<?> getServices() {
+        return new ResponseEntity<>(discoveryClient.getServices(), HttpStatus.OK);
+    }
+
     @PostMapping("/service/registration")
     public ResponseEntity<?> saveUser(@RequestBody User user) {
-        if(userService.findByUsername(user.getUsername()) != null) {
+        if (userService.findByUsername(user.getUsername()) != null) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
         //Default role = user
@@ -33,7 +60,7 @@ public class UserController {
     @GetMapping("/service/login")
     public ResponseEntity<?> getUser(HttpServletRequest request) {
         Principal principal = request.getUserPrincipal();
-        if(principal == null || principal.getName() == null) {
+        if (principal == null || principal.getName() == null) {
             //This means; logout will be successful. login?logout
             return new ResponseEntity<>(HttpStatus.OK);
         }
